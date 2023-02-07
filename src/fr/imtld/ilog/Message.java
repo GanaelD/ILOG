7effde765808cb1318edfc3fd4a8b8b9665f8356
GanaelD <ganael59320@gmail.com>
2023-02-07 14:04:37 +0100

    Changed Message.java: added toString, constructors...

--- src/fr/imtld/ilog/Message.java
+++ src/fr/imtld/ilog/Message.java
@@ -2,11 +2,30 @@ package fr.imtld.ilog;
 
 public class Message {
 	
+	private String dest;
+
+	public Message(String dest) {
+		super();
+		this.dest = dest;
+	}
+	
+	public Message() {
+		this.dest = "Hello";
+	}
+
 	public void display() {
-		System.out.println("Hello World!");
+		System.out.println(this);
 	}
 
 	public void display(String msg) {
 		System.out.printf("Hello %s !\n", msg);
+	}
+
+	public String toString() {
+		return "Hello " + getDest() + " !";
+	}
+
+	public String getDest() {
+		return dest;
 	}
 }
